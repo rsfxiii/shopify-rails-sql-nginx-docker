916ee4bf252dc6de8e7483bf0fb977bb9a2392eb
Raijinn-XIII <ryanshawferguson@gmail.com>
2019-09-18 22:54:11 -0400

    Initial working iteration; without docker

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+require 'shopify_app'
+
+class HomeController < ShopifyApp::AuthenticatedController
+  def index
+    @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
+    @webhooks = ShopifyAPI::Webhook.find(:all)
+  end
+end
